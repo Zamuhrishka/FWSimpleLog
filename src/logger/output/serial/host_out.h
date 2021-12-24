56641bcfde7e2823ef2ccfca467f101d6df6e3e9
Zamuhrishka <roux@yandex.ru>
2021-12-24 16:20:31 +0300

    Add module for log serial output

---
+++ src/logger/output/serial/host_out.h
@@ -0,0 +1,29 @@
+/**
+* \file         FWSimpleDebug.h
+* \author       Kovalchuk Alexander (roux@yandex.ru)
+* \brief        Very simple implementation of debug output.
+*/
+
+#pragma once
+
+/* C++ detection */
+#ifdef __cplusplus
+    extern "C" {
+#endif
+//_____ I N C L U D E S _______________________________________________________
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "logger.h"
+//_____ C O N F I G S  ________________________________________________________
+//_____ D E F I N I T I O N S _________________________________________________
+//_____ M A C R O S ___________________________________________________________
+//_____ V A R I A B L E S _____________________________________________________
+//_____ P U B L I C  F U N C T I O N S_________________________________________
+void logger_output_to_host(const log_msg_t* msg);
+
+/* C++ detection */
+#ifdef __cplusplus
+}
+#endif
