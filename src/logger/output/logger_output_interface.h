470c30905bcf647e16b9bf185c4898192046cce2
Zamuhrishka <roux@yandex.ru>
2021-12-27 18:46:51 +0300

    Add logger output interface

---
+++ src/logger/output/logger_output_interface.h
@@ -0,0 +1,29 @@
+/**
+* \file         FWSimpleDebug.h
+* \author       Kovalchuk Alexander (roux@yandex.ru)
+* \brief        Very simple implementation of debug output.
+*/
+
+#pragma once
+
+/* C++ detection */
+#ifdef __cplusplus
+    extern "C" {
+#endif
+//_____ I N C L U D E S _______________________________________________________
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "dlog.h"
+//_____ C O N F I G S  ________________________________________________________
+//_____ D E F I N I T I O N S _________________________________________________
+typedef bool (*logger_output_fpt)(const dlog_t* msg);
+//_____ M A C R O S ___________________________________________________________
+//_____ V A R I A B L E S _____________________________________________________
+//_____ P U B L I C  F U N C T I O N S_________________________________________
+
+/* C++ detection */
+#ifdef __cplusplus
+}
+#endif
