24d6a77727a463e44451a5ba65fc44e2f50783fd
Alexander <roux@yandex.ru>
2022-01-03 00:44:49 +0300

    [+]Implement interface header for log

--- src/logger/logger.h
+++ src/logger/logger.h
@@ -1,5 +1,5 @@
 /**
-* \file         FWSimpleDebug.h
+* \file         logger.h
 * \author       Kovalchuk Alexander (roux@yandex.ru)
 * \brief        Very simple implementation of debug output.
 */
@@ -11,96 +11,32 @@
     extern "C" {
 #endif
 //_____ I N C L U D E S _______________________________________________________
+#include "logger_core.h"
+
 #include <stdbool.h>
 #include <stdint.h>
-#include <stdlib.h>
-
-#include "dlog.h"
-#include "logger_output_interface.h"
 //_____ C O N F I G S  ________________________________________________________
+#define LOGGER_MESSAGE_MAX_LEN					256
 //_____ D E F I N I T I O N S _________________________________________________
-typedef enum {
-	LOGGER__SILENCE_MODE	=	0,
-	LOGGER__SIMPLE_MODE,															//Only text messages without severity and module
-	LOGGER__FULL_MODE
-} Logger_Mode_e;
-
 //_____ M A C R O S ___________________________________________________________
-#define LOG_INFO(module, fmt, ...)      log(DBG_INFO, module, fmt, __VA_ARGS__)
-#define LOG_WARN(module, fmt, ...)      log(DBG_WARNING, module, fmt, __VA_ARGS__)
-#define LOG_ERROR(module, fmt, ...)     log(DBG_ERROR, module, fmt, __VA_ARGS__)
-#define LOG_FATAL(module, fmt, ...)     log(DBG_CRITICAL, module, fmt, __VA_ARGS__)
+#define LOG_INFO_PARAM(module, fmt, ...)      	log(SEVERITY_INFO, module, fmt, __VA_ARGS__)
+#define LOG_WARN_PARAM(module, fmt, ...)      	log(SEVERITY_WARNING, module, fmt, __VA_ARGS__)
+#define LOG_ERROR_PARAM(module, fmt, ...)     	log(SEVERITY_ERROR, module, fmt, __VA_ARGS__)
+#define LOG_FATAL_PARAM(module, fmt, ...)     	log(SEVERITY_FATAL, module, fmt, __VA_ARGS__)
+
+#define LOG_INFO(module, fmt)      				log(SEVERITY_INFO, module, fmt)
+#define LOG_WARN(module, fmt)      				log(SEVERITY_WARNING, module, fmt)
+#define LOG_ERROR(module, fmt)     				log(SEVERITY_ERROR, module, fmt)
+#define LOG_FATAL(module, fmt)     				log(SEVERITY_FATAL, module, fmt)
+
+#define LOG_DUMP8(buff)     					logger_mem_dump(buff, &buff[0], sizeof(buff)/sizeof(buff[0]), LOGGER_DUMP_WIDTH_8)
+#define LOG_DUMP16(buff)     					logger_mem_dump(buff, &buff[0], sizeof(buff)/sizeof(buff[0]), LOGGER_DUMP_WIDTH_16)
+#define LOG_DUMP32(buff)     					logger_mem_dump(buff, &buff[0], sizeof(buff)/sizeof(buff[0]), LOGGER_DUMP_WIDTH_32)
+
+//TODO: Need to be reworked
+#define LOG_ASSERT(fmt, ...)					LOG_FATAL_PARAM(fmt, __VA_ARGS__, __FILE__, __LINE__)
 //_____ V A R I A B L E S _____________________________________________________
 //_____ P U B L I C  F U N C T I O N S_________________________________________
-/*!
-* \brief 	Initialization of debug subsystem.
-*
-* \param  	none.
-*
-* \return 	true/false.
-*/
-bool logger_init(Logger_Mode_e mode, severity_t severity);
-
-/*!
-* \brief 	Switch on debug.
-*
-* \param  	none.
-*
-* \return 	none.
-*/
-void logger_on(void);
-
-/*!
-* \brief 	Switch off debug.
-*
-* \param  	none.
-*
-* \return 	none.
-*/
-void logger_off(void);
-
-/*!
-* \brief 	Setup debug level.
-*
-* \param[in]  level: active level of debug.
-* 			DBG_INFO - allow all type of debug message;
-* 			DBG_WARNING - common debug messages switch off;
-* 			DBG_ERROR - warning and common debug messages switch off;
-* 			DBG_CRITICAL - allow only critical debug messages;
-*
-* \return 	none.
-*/
-void logger_set_severity(severity_t severity);
-
-/*!
-* \brief 	Return debug level.
-*
-* \param  	none.
-*
-* \return 	active level of debug.
-*/
-severity_t logger_get_severity(void);
-
-/*!
-* \brief 	Register special function for print debug message.
-*
-* \param[in]  out: output function
-*
-* \return 	none.
-*/
-void logger_output_register(logger_output_fpt out);
-
-/*!
-* \brief 	Print debug message into console.
-*
-* \param[in]  level: active level of debug.
-* \param[in]  fmt: debug string.
-*
-* \return 	none.
-*/
-void log(severity_t severity, const char* module, const char* fmt, ...);
-
-void logr(const char* fmt, ...);
 
 /* C++ detection */
 #ifdef __cplusplus
